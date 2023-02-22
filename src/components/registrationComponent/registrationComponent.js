10e7e23b2c1e7422a3e1db4bcf1ab5b123f013a6
MalyshevaElena <ger.malysheva@yandex.ru>
2023-02-22 18:21:54 +0700

    Tried Registration test

--- src/components/registrationComponent/registrationComponent.js
+++ src/components/registrationComponent/registrationComponent.js
@@ -1,5 +1,22 @@
 import './registrationComponent.css';
+
 function Registration() {
+    let test = async function(){
+        fetch('http://localhost:3030/rest/auth/registration', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json; charset=UTF-8',
+              },
+            method: 'POST',
+            mode: "no-cors",
+            body: JSON.stringify({
+                email: 'testemail',
+                password: 'testpassword',
+            })
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    }
     return (
         <div className='registration'>
             <div className='registration__inner'>
@@ -7,7 +24,7 @@ function Registration() {
                 <div className='registration__name'>Имя пользователя</div>
                 <div className='registration__password'>Пароль</div>
                 <div className='registration__password--checking'>Подтвердить пароль</div>
-                <div className='registration__enter'>Зарегистрироваться</div>
+                <button className='registration__enter' onClick={test}>Зарегистрироваться</button>
             </div>
         </div>
     )
